e87ff0d02fdf408bd3867e0347ae8bf90e0926fe
Micah Halter <micah@mehalter.com>
2023-05-10 17:59:10 -0400

    move literate into docs folder

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,33 @@
 using Documenter
 using Literate
 
-@info "Loading RegNets"
-using RegNets
-
-# Set Literate.jl config if not being compiled on recognized service.
-config = Dict{String,String}()
-if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
-  config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/AlgebraicJulia/RegNets.jl/blob/gh-pages/dev"
-  config["repo_root_url"] = "https://github.com/AlgebraicJulia/RegNets.jl/blob/master/docs"
-end
-
-const literate_dir = joinpath(@__DIR__, "..", "examples")
+const literate_dir = joinpath(@__DIR__, "literate")
 const generated_dir = joinpath(@__DIR__, "src", "examples")
 
-for (root, dirs, files) in walkdir(literate_dir)
-  out_dir = joinpath(generated_dir, relpath(root, literate_dir))
-  for file in files
-    f,l = splitext(file)
-    if l == ".jl" && !startswith(f, "_")
-      Literate.markdown(joinpath(root, file), out_dir;
-        config=config, documenter=true, credit=false)
-      Literate.notebook(joinpath(root, file), out_dir;
-        execute=true, documenter=true, credit=false)
+@info "Loading RegNets"
+using RegNets, RegNets.ASKEMRegNets, RegNets.SignedPetriNets
+
+const no_literate = "--no-literate" in ARGS
+if !no_literate
+  @info "Building Literate.jl docs"
+
+  # Set Literate.jl config if not being compiled on recognized service.
+  config = Dict{String,String}()
+  if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
+    config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/AlgebraicJulia/RegNets.jl/blob/gh-pages/dev"
+    config["repo_root_url"] = "https://github.com/AlgebraicJulia/RegNets.jl/blob/master/docs"
+  end
+
+  for (root, dirs, files) in walkdir(literate_dir)
+    out_dir = joinpath(generated_dir, relpath(root, literate_dir))
+    for file in files
+      f,l = splitext(file)
+      if l == ".jl" && !startswith(f, "_")
+        Literate.markdown(joinpath(root, file), out_dir;
+          config=config, documenter=true, credit=false)
+        Literate.notebook(joinpath(root, file), out_dir;
+          execute=true, documenter=true, credit=false)
+      end
     end
   end
 end
@@ -39,7 +44,7 @@ makedocs(
   pages     = Any[
     "RegNets.jl" => "index.md",
     "Examples" => Any[
-      "examples/signedgraphs/SignedGraphs.md",
+      "generated/SignedGraphs.md",
     ],
     "Library Reference" => "api.md",
   ]
