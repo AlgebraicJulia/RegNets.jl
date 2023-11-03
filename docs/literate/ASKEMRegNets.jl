8d0181e4548fb619740a5a9c3321abc7a37e1771
Jadon Clugston <34165782+jClugstor@users.noreply.github.com>
2023-11-02 21:43:41 -0400

    Update ASKEMRegNets.jl
    Make docs show actual parsing function

--- docs/literate/ASKEMRegNets.jl
+++ docs/literate/ASKEMRegNets.jl
@@ -72,33 +72,31 @@ to_graphviz(SchASKEMRegNet, edge_attrs=Dict(:len=>"1.5"))
 #
 # ```julia
 # function parse_askem_model(input::AbstractDict)
-#   regnet = ASKEMRegNet()
-#   param_vals = Dict(p["id"]=>p["value"] for p in input["model"]["parameters"])
-#   resolve_val(x) = typeof(x) == String ? param_vals[x] : x
-# 
-#   vertice_idxs = Dict(vertice["id"]=> add_part!(regnet, :V;
-#     vname=Symbol(vertice["id"]),
-#     vrate = 0
-#     if haskey(vertice, "rate_constant")
-#       vrate = (vertice["sign"] ? 1 : -1) * resolve_val(vertice["rate_constant"])
-#     end
-#     initial=haskey(vertice, "initial") ? resolve_val(vertice["initial"]) : 0
-#   ) for vertice in input["model"]["vertices"])
-# 
-#   for edge in input["model"]["edges"]
-#     rate = 0
-#     if haskey(edge, "properties") && haskey(edge["properties"], "rate_constant")
-#       rate = resolve_val(edge["properties"]["rate_constant"])
-#       rate >= 0 || error("Edge rates must be strictly positive")
-#     end
-#     add_part!(regnet, :E; src=vertice_idxs[edge["source"]],
-#                           tgt=vertice_idxs[edge["target"]],
-#                           sign=edge["sign"],
-#                           ename=Symbol(edge["id"]),
-#                           erate=rate)
-#   end
-# 
-#   regnet
+#function parse_askem_model(input::AbstractDict)
+#  regnet = ASKEMRegNet()
+#  param_vals = Dict(p["id"]=>p["value"] for p in input["model"]["parameters"])
+#  resolve_val(x) = typeof(x) == String ? param_vals[x] : x
+#
+#  vertice_idxs = Dict(vertice["id"]=> add_part!(regnet, :V;
+#    vname=Symbol(vertice["id"]),
+#    vrate=haskey(vertice, "rate_constant") ? (vertice["sign"] ? 1 : -1) * resolve_val(vertice["rate_constant"]) : 0,
+#    initial=haskey(vertice, "initial") ? resolve_val(vertice["initial"]) : 0
+#  ) for vertice in input["model"]["vertices"])
+#
+#  for edge in input["model"]["edges"]
+#    rate = 0
+#    if haskey(edge, "properties") && haskey(edge["properties"], "rate_constant")
+#      rate = resolve_val(edge["properties"]["rate_constant"])
+#      rate >= 0 || error("Edge rates must be strictly positive")
+#    end
+#    add_part!(regnet, :E; src=vertice_idxs[edge["source"]],
+#                          tgt=vertice_idxs[edge["target"]],
+#                          sign=edge["sign"],
+#                          ename=Symbol(edge["id"]),
+#                          erate=rate)
+#  end
+#
+#  regnet
 # end
 # ```
 
